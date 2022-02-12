45044775d3fd478086b9860329c28a37a2ce6932
erb4f1dotama <erb4fidotama@gmail.com>
2022-02-12 12:24:08 +0700

    testing konek ke local eposyandu backend

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 
 import useStore from "../../store/Home";
 
@@ -29,16 +29,17 @@ const Index = () => {
   }));
 
   const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
+    "&:nth-of-type(odd)": {
       backgroundColor: theme.palette.action.hover,
     },
     // hide last border
-    '&:last-child td, &:last-child th': {
+    "&:last-child td, &:last-child th": {
       border: 0,
     },
   }));
 
   useEffect(() => {
+    console.log("1");
     fetchUser();
   }, []);
 
@@ -52,62 +53,73 @@ const Index = () => {
   };
 
   const columns = [
-    { id: "name", label: "Name" },
-    { id: "username", label: "Username" },
-    { id: "website", label: "Website" },
-    { id: "email", label: "Email" },
+    { id: "lookup_code", label: "Lookup Code" },
+    { id: "description", label: "Description" },
+    // { id: "website", label: "Website" },
+    // { id: "email", label: "Email" },
   ];
 
   console.log(dataUsers);
   return (
     <div>
       <p>Ini Home Index</p>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <StyledTableCell
-                  key={column.id}
-                  align={column.align}
-                  // style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </StyledTableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {dataUsers
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <StyledTableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === "number"
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </StyledTableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={dataUsers.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
+      {dataUsers.data ? (
+        <>
+          <TableContainer sx={{ maxHeight: 440 }}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  {columns.map((column) => (
+                    <StyledTableCell
+                      key={column.id}
+                      align={column.align}
+                      // style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </StyledTableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {dataUsers?.data
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => {
+                    return (
+                      <StyledTableRow
+                        hover
+                        role="checkbox"
+                        tabIndex={-1}
+                        key={row.code}
+                      >
+                        {columns.map((column) => {
+                          const value = row[column.id];
+                          return (
+                            <TableCell key={column.id} align={column.align}>
+                              {column.format && typeof value === "number"
+                                ? column.format(value)
+                                : value}
+                            </TableCell>
+                          );
+                        })}
+                      </StyledTableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={dataUsers?.data?.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </>
+      ) : (
+        <h4>LOADING</h4>
+      )}
     </div>
   );
 };
--- src/store/Home.js
+++ src/store/Home.js
@@ -4,7 +4,8 @@ import axios from "axios";
 const useStore = create((set) => ({
   users: [],
   fetchUser: async (payload) => {
-    const url = "https://jsonplaceholder.typicode.com/users";
+    // const url = "https://jsonplaceholder.typicode.com/users";
+    const url = "http://localhost:8080/lookup/"
     const response = await axios.get(url);
     set({ users: await response.data });
   },
